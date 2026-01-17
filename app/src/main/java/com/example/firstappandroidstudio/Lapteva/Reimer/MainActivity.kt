4967618f1ee96d441965f0b4bbcd4e1e4570117b
TT-lgtm <comkosmos.sch@gmail.com>
2026-01-17 14:42:02 +0300

    AppAndroidStudio_Done

--- app/src/main/java/com/example/firstappandroidstudio/Lapteva/Reimer/MainActivity.kt
+++ app/src/main/java/com/example/firstappandroidstudio/Lapteva/Reimer/MainActivity.kt
@@ -5,6 +5,9 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.widget.ImageView
+import android.widget.Button
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +18,20 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val imageDice: ImageView = findViewById(R.id.imageView)
+        val buttonRoll: Button = findViewById(R.id.button_start)
+        val diceImages = intArrayOf(
+            R.drawable.dice_1,
+            R.drawable.dice_2,
+            R.drawable.dice_3,
+            R.drawable.dice_4,
+            R.drawable.dice_5,
+            R.drawable.dice_6
+        )
+        buttonRoll.setOnClickListener {
+            val randomNumber = Random.nextInt(1, 7)
+            imageDice.setImageResource(diceImages[randomNumber - 1])
         }
     }
 }
